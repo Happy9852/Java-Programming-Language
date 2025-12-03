159c5f665ecaa5c229917654b6df51b8eb747740
Happy9852 <archnemesis609@gmail.com>
2025-12-03 22:39:23 +0530

    pattern programs are modified by sai

--- patterns-java/pattern_21.java
+++ patterns-java/pattern_21.java
@@ -26,7 +26,6 @@ public class pattern_21 {
             }
             System.out.println();
         }
-
         for (int i=n-1; i>=1; i--) {
             for (int j=1; j<=n-i; j++) {
                 System.out.print("  ");
